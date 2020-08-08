721e8c1fb744e90aff8258fde26aac243610143f
lrwagner <louisr.wagner@gmail.com>
2020-08-08 18:47:07 +0200

    created base template
     Changes to be committed:
    	modified:   song_webapp/song_request/models.py
    	modified:   song_webapp/song_request/views.py
    	modified:   song_webapp/song_webapp/urls.py
    
     Untracked files:
    	song_webapp/db.sqlite3
    	song_webapp/song_request/templates/
    		song_webapp/song_webapp/__pycache__/urls.cpython-37.pyc

--- song_webapp/song_request/models.py
+++ song_webapp/song_request/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 
-class Song(models.Model):
-    name = models.CharField(max_length=40)
-    album = models.CharField(max_length=40)
-    band = models.ForeignKey(Band, on_delete=models.CASCADE)
-
-
 class Band(models.Model):
     name = models.CharField(max_length=40)
 
 
+class Song(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Titel')
+    album = models.CharField(max_length=40, verbose_name='Album')
+    year = models.DateField(verbose_name='Erscheinungsjahr')
+    band = models.ForeignKey(Band, verbose_name='Band', on_delete=models.CASCADE)
+
+
 class Rehearsal(models.Model):
-    date = models.DateField()
-    songs = models.ManyToManyField(Song)
+    date = models.DateField(verbose_name='Datum')
+    songs = models.ManyToManyField(Song, verbose_name='Songs')
