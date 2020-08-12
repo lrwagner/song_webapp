08eab76534d02a48bcb40ceb95e76ea8f4c00cee
lrwagner <louisr.wagner@gmail.com>
2020-08-12 18:41:38 +0200

    updated templates and backend for forms

--- song_webapp/song_request/models.py
+++ song_webapp/song_request/models.py
@@ -19,20 +19,33 @@ class Song(models.Model):
     info = models.TextField(verbose_name='Info')
 
     @property
+    def rehearsed_count(self):
+        rehearsal_query = Rehearsal.objects.filter(songs__name=self.name)
+        if rehearsal_query.exists():
+            return rehearsal_query.count()
+        else:
+            return 0
+
+    @property
     def last_played(self):
         rehearsal_query = Rehearsal.objects.filter(songs__name=self.name)
-
         if rehearsal_query.exists():
-            days_not_played = datetime.date.today() - rehearsal_query.latest('date').date
-            if days_not_played.days < 15:
-                return 'green'
-            elif 20 > days_not_played.days > 15:
-                return 'yellow' 
-            else:
-                return 'orange'
+            return rehearsal_query.latest('date').date
         else:
+            return 'Nie'
+    
+    @property
+    def color(self):
+        if self.last_played == 'Nie':
             return 'red'
-
+        else:
+            days_not_played = datetime.date.today() - self.last_played
+        if days_not_played.days < 15:
+            return 'green'
+        elif 20 > days_not_played.days > 15:
+            return 'yellow' 
+        else:
+            return 'orange'
 
     def __str__(self):
         return self.name
--- song_webapp/song_request/views.py
+++ song_webapp/song_request/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.db.models import Count
+from django.http import HttpResponseRedirect
+
 from song_request import models
+from .forms import NameForm
+
 
 def song_view(request):
 
@@ -28,5 +32,18 @@ def rehearsal_view(request):
 
 
 def create_rehearsal_view(request):
+        # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/probe/')
 
-    return render(request, 'song_request/create_rehearsal.html')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+    return render(request, 'song_request/create_rehearsal.html', {'form': form})
